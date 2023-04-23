dbd996aad39eaa0aabb28979a2c5a9dd5594b510
Rotem <rotemgeva123@gamil.com>
2023-04-22 23:28:10 -0700

    22.4 project working, no errors. problems with the date picker methods

--- app/src/main/java/com/example/myapplication_final/LessonTypes.java
+++
@@ -1,5 +0,0 @@
-package com.example.myapplication_final;
-
-public enum LessonTypes {
-
-}
--- app/src/main/java/com/example/myapplication_final/MainScreen.java
+++ app/src/main/java/com/example/myapplication_final/MainScreen.java
@@ -4,22 +4,31 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainScreen extends AppCompatActivity implements View.OnClickListener {
     ListView lv_insert_lesson;
     ArrayList<Student> students_list;
     StudentAdapter adapter;
+
+    TextView datePicker;
     AlertDialog.Builder builder;
+
+    //stam us used in order to go to ShowStudents activity for a while
     Button stam;
 
 
@@ -50,6 +59,62 @@ public class MainScreen extends AppCompatActivity implements View.OnClickListene
         });
 
         lv_insert_lesson.setAdapter(adapter);
+
+
+        /**
+         * date picker
+         */
+
+
+
+        // on below line we are initializing our variables.
+
+        datePicker = findViewById(R.id.date_picker_main);
+        Date date = new Date();
+
+        // on below line we are adding click listener for our pick date button
+        datePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // on below line we are getting
+                // the instance of our calendar.
+                final Calendar c = Calendar.getInstance();
+
+                // on below line we are getting
+                // our day, month and year.
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+
+                // on below line we are creating a variable for date picker dialog.
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        // on below line we are passing context.
+
+                        MainScreen.this, new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                          int dayOfMonth) {
+                        // on below line we are setting date to our text view.
+                        date.setDate(dayOfMonth);
+                        date.setMonth(monthOfYear);
+                        date.setYear(year);
+                        datePicker.setText(date.getDay() + "-" + (date.getMonth() + 1) +
+                                "-" + date.getYear());
+
+                    }
+                },
+                        // on below line we are passing year,
+                        // month and day for selected date in our date picker.
+                        year, month, day);
+
+                // at last we are calling show to
+                // display our date picker dialog.
+
+                datePickerDialog.show();
+            }
+        });
 
 
 
